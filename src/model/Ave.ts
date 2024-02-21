49a603b927e4a1af6de1fa8d646f85548d5d7167
Rafael Seles de Souza <rafa30262317@gmail.com>
2024-02-21 15:28:34 -0300

    commitando dnv

--- src/model/Ave.ts
+++ src/model/Ave.ts
@@ -1,27 +1,85 @@
+import { globalAgent } from "http";
 import { Animal } from "./Animal";
+import { DatabaseModel } from "./Databasemodel";
 
+const database = new DatabaseModel().pool;
+
+/**
+ * Representa um ave no zoológico, que é uma subclasse de Animal.
+ */
 export class Ave extends Animal {
+    /**
+     * A envergadura da ave.
+     */
     private envergadura: number;
-    constructor(_nome: string, _envergadura: number, _genero: string, _idade: number) {
+
+    /**
+     * Cria uma nova instância de ave.
+     *
+     * @param _nome O nome do ave.
+     * @param _idade A idade do ave.
+     * @param _genero O gênero do ave.
+     * @param _envergadura A envergadura da ave.
+     */
+    constructor(_nome: string,
+                _idade: number,
+                _genero: string,
+                _envergadura: number) {
         super(_nome, _idade, _genero);
         this.envergadura = _envergadura;
     }
 
     /**
-      * Retorna a envergadura do animal
-      *
-      * @returns envergadura : envergadura do animal
-      */
+     * Obtém A envergadura da ave.
+     *
+     * @returns A envergadura da ave.
+     */
     public getEnvergadura(): number {
         return this.envergadura;
     }
 
     /**
-     * Atribui o parâmetro à envergadura
+     * Define A envergadura da ave.
      *
-     * @param _envergadura : envergadura do animal
+     * @param _envergadura A envergadura a ser atribuído ao ave.
      */
     public setEnvergadura(_envergadura: number): void {
         this.envergadura = _envergadura;
+    }
+
+    static async listarAves() {
+        const listaDeAves: Array<Ave> = [];
+        try {
+            const queryReturn = await database.query(`SELECT * FROM  ave;`);
+            queryReturn.rows.forEach(ave => {
+                listaDeAves.push(ave);
+            });
+
+            // só pra testar se a lista veio certa do banco
+            console.log(listaDeAves);
+
+            return listaDeAves;
+        } catch (error) {
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error";
+        }
+    }
+
+    static async cadastrarAve(Ave: Ave): Promise<any> {
+        try {
+            let insertResult = false;
+            await database.query(`INSERT INTO ave (nome, idade, genero, envergadura)
+                VALUES
+                ('${Ave.getNome().toUpperCase()}', ${Ave.getIdade()}, '${Ave.getGenero().toUpperCase()}', ${Ave.getEnvergadura()});
+            `).then((result) => {
+                if(result.rowCount != 0) {
+                    insertResult = true;
+                }
+            });
+            return insertResult;
+        } catch(error) {
+            return error;
+        }
     }
 }
--- src/model/mamifero.ts
+++ src/model/mamifero.ts
@@ -1,27 +1,85 @@
+import { globalAgent } from "http";
 import { Animal } from "./Animal";
+import { DatabaseModel } from "./Databasemodel";
 
-export class Mamifero extends Animal{
+const database = new DatabaseModel().pool;
+
+/**
+ * Representa um mamífero no zoológico, que é uma subclasse de Animal.
+ */
+export class Mamifero extends Animal {
+    /**
+     * A raça do mamífero.
+     */
     private raca: string;
-    constructor( _nome: string, _idade: number, _genero: string, _raca: string){
+
+    /**
+     * Cria uma nova instância de Mamifero.
+     *
+     * @param _nome O nome do mamífero.
+     * @param _idade A idade do mamífero.
+     * @param _genero O gênero do mamífero.
+     * @param _raca A raça do mamífero.
+     */
+    constructor(_nome: string,
+                _idade: number,
+                _genero: string,
+                _raca: string) {
         super(_nome, _idade, _genero);
-        this.raca = _raca
+        this.raca = _raca;
     }
 
     /**
-     * Retorna a raça do animal
+     * Obtém A raça do mamífero.
      *
-     * @returns raca : raça do animal
+     * @returns A raça do mamífero.
      */
     public getRaca(): string {
         return this.raca;
     }
 
     /**
-     * Atribui o parâmetro ao atributo animal
+     * Define A raça do mamífero.
      *
-     * @param _raca : raça do animal
+     * @param _raca A raça a ser atribuído ao mamífero.
      */
     public setRaca(_raca: string): void {
         this.raca = _raca;
+    }
+
+    static async listarMamiferos() {
+        const listaDeMamiferos: Array<Mamifero> = [];
+        try {
+            const queryReturn = await database.query(`SELECT * FROM  mamifero;`);
+            queryReturn.rows.forEach(mamifero => {
+                listaDeMamiferos.push(mamifero);
+            });
+
+            // só pra testar se a lista veio certa do banco
+            console.log(listaDeMamiferos);
+
+            return listaDeMamiferos;
+        } catch (error) {
+            console.log('Erro no modelo');
+            console.log(error);
+            return "error";
+        }
+    }
+
+    static async cadastrarMamifero(Mamifero: Mamifero): Promise<any> {
+        try {
+            let insertResult = false;
+            await database.query(`INSERT INTO mamifero (nome, idade, genero, raca)
+                VALUES
+                ('${Mamifero.getNome().toUpperCase()}', ${Mamifero.getIdade()}, '${Mamifero.getGenero().toUpperCase()}', '${Mamifero.getRaca().toUpperCase()}');
+            `).then((result) => {
+                if(result.rowCount != 0) {
+                    insertResult = true;
+                }
+            });
+            return insertResult;
+        } catch(error) {
+            return error;
+        }
     }
 }
